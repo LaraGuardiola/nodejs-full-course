dcabed329b570982273e36b30b58739e38077e78
Sergio <webdev.slara@gmail.com>
2022-06-14 17:17:29 +0200

    worked with the filesystem module fs in order to read and write txt files

--- server.js
+++ server.js
@@ -4,19 +4,48 @@
 const obj = require('./module.js')
 const os = require('os')
 const path = require('path')
-const { readFileSync, writeFileSync } = require('fs') //in order to read files from the pc
+const { readFileSync, writeFileSync, writeFile, readFile } = require('fs') //in order to read files from the pc
+
+//asyncronous way to read and write files
+readFile('content/first.txt', (err, result) => {
+    if(err){
+        console.log(err)
+        return
+    }
+    const one = result
+    readFile('./content/subfolder/second.txt','utf8', (err, result) => {
+        if(err){
+            console.log(err)
+            return 
+        }
+        const two = result
+        writeFile(
+            './content/result_async.txt',
+            `Here is the result: ${one}, ${two}`,
+            (err, result) => {
+                if(err){
+                    console.log(err)
+                    return
+                }
+                console.log(result)
+            }
+        )
+    })
+})
+
+//syncronous way to write files
 
 //reads the txt files inside the folders
 const first = readFileSync('./content/first.txt', 'utf8')
 const second = readFileSync('./content/subfolder/second.txt', 'utf8')
 console.log(first, second)
 
-//syncronous way to write files
 writeFileSync(
     './content/result_sync.txt',
     `Here is the result: ${first}, ${second}`,
     { flag: 'a'}
 )
+
 //* GLOBAL VARIABLES
 // __dirname    - path to current directory
 // __filename   - path to current file
@@ -26,16 +55,16 @@ writeFileSync(
 
 
 //testing quick snippet in node js
-const flag = false;
+const flag = false
 
-flag ? console.log('es true') : console.log(); //10 from module.js
+flag ? console.log('es true') : console.log() //10 from module.js
 
 let {age, name} = obj
 console.log(age,name)
 /*for(let elem of Object.values(obj)){
     console.log(elem)
 }*/
-console.log(os.platform()); //win32
+console.log(os.platform()) //win32
 
 console.log(path.sep) // \
 console.log(path.join('node_modules','.bin')) // node_modules\.bin
