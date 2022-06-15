074c552b1d9ee2cfa5f2f18c7c8b2049d45b8281
Sergio <webdev.slara@gmail.com>
2022-06-15 10:49:41 +0200

    added an IIFE for fetching data in axios.js, also added the same method but destructuring data

--- axios.js
+++ axios.js
@@ -9,9 +9,25 @@ const axios = require('axios');
 bro()*/
 
 //inside an IIFE
-
-(async () => {
+/*(async () => {
     console.log('bro')
-    const { strain, type, cannabinoid } = await axios.get('https://random-data-api.com/api/cannabis/random_cannabis?size=1')
-    console.log(strain, type, cannabinoid)
-})()
+    const result = await axios.get('https://random-data-api.com/api/cannabis/random_cannabis?size=1')
+    console.log(result)
+})()*/
+
+class GuardiaCivil {
+    constructor(name, age){
+        this.name = name
+        this.age = age
+    }
+    //GC fetches illegal substances
+    async commandeer(){
+        const result = await axios.get('https://random-data-api.com/api/cannabis/random_cannabis?size=1')
+        let { strain, cannabinoid } = result.data[0]
+        console.log(strain, cannabinoid)
+    }
+}
+
+let JC = new GuardiaCivil('jc', 30)
+setTimeout(() => console.log(JC.age, JC.name), 5000)
+JC.commandeer()
